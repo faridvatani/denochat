419a4a2fe1d05b1122c3d938a7c29665123f9281
mohammadEslamiyeh <mohammades6634@yahoo.com>
2021-07-09 19:48:54 +0430

    create modal for logout from account

--- plugins/main.js
+++ plugins/main.js
@@ -6,3 +6,16 @@ function login() {
     }
 
 }
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function logout() {
+    location.replace("login.html")
+}
