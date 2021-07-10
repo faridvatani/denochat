9e6f887a8d8621edfdf816d679e59c778dfd2355
mohammadEslamiyeh <mohammades6634@yahoo.com>
2021-07-10 10:56:47 +0430

    add emoji and set style

--- plugins/main.js
+++ plugins/main.js
@@ -19,3 +19,28 @@ window.onclick = function(event) {
 function logout() {
     location.replace("login.html")
 }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+$(function() {
+    // Initializes and creates emoji set from sprite sheet
+    window.emojiPicker = new EmojiPicker({
+        emojiable_selector: '[data-emojiable=true]',
+        assetsPath: 'http://onesignal.github.io/emoji-picker/lib/img/',
+        popupButtonClasses: 'far fa-laugh-beam'
+    });
+    // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+    // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+    // It can be called as many times as necessary; previously converted input fields will not be converted again
+    window.emojiPicker.discover();
+});
