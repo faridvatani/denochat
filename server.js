6d9ae03b9ac9ec7d6bfa626421ea0d5cd897c637
farid landi <faridvatani@gmail.com>
2021-11-22 19:00:00 +0100

    Bug Fix - HTTP Connection Error

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 
-import { listenAndServe } from "https://deno.land/std/http/server.ts";
-import { acceptWebSocket, acceptable } from "https://deno.land/std/ws/mod.ts";
+import { listenAndServe } from "https://deno.land/std@0.58.0/http/server.ts";
+import { acceptWebSocket, acceptable } from "https://deno.land/std@0.58.0/ws/mod.ts";
 import chat from "./chat.js";
 
 listenAndServe({ port: 3000 }, async (req) => {
+  if (req.method === "GET" && req.url === "/ws") {
     if (acceptable(req)) {
       acceptWebSocket({
         conn: req.conn,
         bufReader: req.r,
         bufWriter: req.w,
         headers: req.headers,
-      })
-      .then(chat);
+      }).then(chat);
     }
+  }
 });
-
 console.log("Server started on port 3000");
