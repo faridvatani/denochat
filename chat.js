595278620a5a121a0994514b0c6f032a112a32fe
farid landi <faridvatani@gmail.com>
2021-11-26 23:14:23 +0330

    Connection methods for clients

--- chat.js
+++ chat.js
@@ -1,3 +1,16 @@
+// @ts-nocheck
+import { isWebSocketCloseEvent } from "https://deno.land/std@0.65.0/ws/mod.ts";
+import { v4 } from "https://deno.land/std@0.65.0/uuid/mod.ts";
+
+// This is called when user is connected
 export default async function chat(ws) {
-    console.log(`Connected`);
+  // Generate unique userId
+  const userId = v4.generate();
+
+  // Listening of WebSocket events
+  for await (let data of ws) {
+    const event = typeof data === "string" ? JSON.parse(data) : data;
+  }
 }
+
+
--- client.js
+++ client.js
@@ -1,36 +1,103 @@
+// @ts-nocheck
 let ws;
+let chatUsersCtr = document.querySelector("#chatUsers");
+let chatUsersCount = document.querySelector("#chatUsersCount");
+let sendMessageForm = document.querySelector("#messageSendForm");
+let messageInput = document.querySelector("#messageInput");
+let chatMessagesCtr = document.querySelector("#chatMessages");
+let leaveGroupBtn = document.querySelector("#leaveGroupBtn");
+let groupName = document.querySelector("#groupName");
 
 window.addEventListener("DOMContentLoaded", () => {
-    ws = new WebSocket(`ws://localhost:3000/ws`);
-    ws.addEventListener("open", onConnectionOpen);
-    ws.addEventListener("message", onMessageReceived);
+  ws = new WebSocket(`${window.location.protocol === 'http:' ? 'ws' : 'wss'}://${window.location.host}/ws`);
+  ws.addEventListener("open", onConnectionOpen);
+  ws.addEventListener("message", onMessageReceived);
 });
 
-function getQueryParams(){
+function getQueryParams() {
     const search = window.location.search.substring(1);
     const pairs = search.split("&");
     const params = {};
-
     for (const pair of pairs) {
-        const items = pair.split("=");
-        params[decodeURIComponent(items[0])] = decodeURIComponent(items[1]);
+      const parts = pair.split("=");
+      params[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
     }
-
+  
     return params;
 }
 
-function onConnectionOpen(){
-    console.log(`Connection Opened`);
-    const queryParams = getQueryParams();
-    // console.log(queryParams);
-    const event ={
-        event: 'Join',
-        groupName: queryParams.group,
-        groupName: queryParams.name
-    }
-    ws.send(event);
+sendMessageForm.onsubmit = (ev) => {
+  ev.preventDefault();
+  if (!messageInput.value){
+    return;
+  }
+  const event = {
+    event: "message",
+    data: messageInput.value,
+  };
+  ws.send(JSON.stringify(event));
+  messageInput.value = "";
+};
+
+leaveGroupBtn.onclick = () => {
+  window.location.href = 'chat.html';
 }
 
-function onMessageReceived(event){
-    console.log("Message received", event);
+function onConnectionOpen() {
+  console.log(`Connection Opened`);
+  const queryParams = getQueryParams();
+  if (!queryParams.name || !queryParams.group) {
+    window.location.href = "chat.html";
+    return;
+  }
+  groupName.innerHTML = queryParams.group;
+  const event = {
+    event: "join",
+    groupName: queryParams.group,
+    name: queryParams.name,
+  };
+  ws.send(JSON.stringify(event));
+}
+
+function onMessageReceived(event) {
+  console.log("Message received ");
+  event = JSON.parse(event.data);
+  console.log(event);
+  switch (event.event) {
+    case "users":
+      chatUsersCount.innerHTML = event.data.length;
+      chatUsersCtr.innerHTML = "";
+      event.data.forEach((u) => {
+        const userEl = document.createElement("div");
+        userEl.className = "chat-user";
+        userEl.innerHTML = u.name;
+        chatUsersCtr.appendChild(userEl);
+      });
+      break;
+    case "message":
+      const el = chatMessagesCtr;
+      const scrollToBottom = Math.floor(el.offsetHeight + el.scrollTop) === el.scrollHeight
+      appendMessage(event.data);
+      
+      if (scrollToBottom) {
+
+        el.scrollTop = 10000000;
+      }
+      break;
+
+    case "previousMessages":
+      event.data.forEach(appendMessage);
+  }
+}
+
+function appendMessage(message) {
+  const messageEl = document.createElement("div");
+  messageEl.className = `message message-${
+    message.sender === "me" ? "to" : "from"
+  }`;
+  messageEl.innerHTML = `
+        ${message.sender === "me" ? "" : `<h4>${message.name}</h4>`}
+        <p class="message-text">${message.message}</p>
+      `;
+  chatMessagesCtr.appendChild(messageEl);
 }
